30ddb6460ed662205160ad9fb83e1224e7d82235
Kadete <fkadete@gmail.com>
2014-10-11 17:58:52 +0100

    preferences test added

---
+++ app/src/main/java/com/example/kadete/contactmanager/QuickPrefsActivity.java
@@ -0,0 +1,52 @@
+package com.example.kadete.contactmanager;
+
+/**
+ * Created by Kadete on 08/10/2014.
+ */
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class QuickPrefsActivity extends PreferenceActivity {
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+//        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 0, 0, "Show current settings");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                startActivity(new Intent(this, ShowSettingsActivity.class));
+                return true;
+        }
+        return false;
+    }
+
+}
